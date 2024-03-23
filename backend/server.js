405a3a677cef21e059f9bf0f0c7ffea2c8104307
PhucLee14 <lhphucth14@gmail.com>
2024-03-23 23:53:00 +0700

    change package.json

--- backend/server.js
+++ backend/server.js
@@ -1,13 +1,14 @@
+import path from "path";
 import express from "express";
 import dotenv from "dotenv";
 import cookieParser from "cookie-parser";
 
 import authRoutes from "./routes/auth.routes.js";
-import messsageRoutes from "./routes/message.routes.js";
+import messageRoutes from "./routes/message.routes.js";
 import userRoutes from "./routes/user.routes.js";
+
 import connectToMongoDB from "./db/connectToMongoDB.js";
 import { app, server } from "./socket/socket.js";
-import path from "path";
 
 const PORT = process.env.PORT || 5000;
 
@@ -15,11 +16,11 @@ const __dirname = path.resolve();
 
 dotenv.config();
 
-app.use(express.json());
+app.use(express.json()); // to parse the incoming requests with JSON payloads (from req.body)
 app.use(cookieParser());
 
 app.use("/api/auth", authRoutes);
-app.use("/api/messages", messsageRoutes);
+app.use("/api/messages", messageRoutes);
 app.use("/api/users", userRoutes);
 
 app.use(express.static(path.join(__dirname, "/frontend/dist")));
